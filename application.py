ec3385df7446720e98c82c468635829285a33cbf
Michael Carey <michael.carey@agilebits.com>
2024-02-07 13:01:54 -0400

    visual changes

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 import stripe
+import json
 from flask import Flask, jsonify, render_template
 from werkzeug.urls import quote as url_quote
 
@@ -19,6 +20,10 @@ def get_products():
         # Convert Stripe's response to a list of dictionaries simplifying the output
         products_list = [{'id': product['id'], 'name': product['name']} for product in products.auto_paging_iter()]
         
+        # Pretty print the JSON data with an indent of 4 spaces
+        pretty_products = json.dumps(products_list, indent=4)
+
+
         # Use render_template to serve your HTML page with the pretty printed JSON
         return render_template('index.html', data=products_list)
     except stripe.error.StripeError as e:
